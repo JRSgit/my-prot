52ea383bce344ec750ad26fc1e1526af7f118086
JRSProg <34550441+JRSgit@users.noreply.github.com>
2025-04-01 21:26:05 -0300

    Update Footer.jsx
    Adicionei Button do whatsapp

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { Button } from "@/components/ui/button";
+import { FaWhatsapp } from "react-icons/fa";
 
 function Footer() {
+  const WhatsAppButton = () => {
+  const phoneNumber = "5573981424418";
+  const message = "Gostaria de criar meu site.";
+  
+  const openWhatsApp = () => {
+    const encodedMessage = encodeURIComponent(message);
+    window.open(`https://wa.me/${phoneNumber}?text=${encodedMessage}`, "_blank");
+  };
   return (
     <div className='mt-10 relative text-white flex flex-col items-center justify-start  w-full h-[200px] border-t-2 border-t-cor-blueTheme'>
-      <p className='mt-5 flex flex-col items-center justify-center w-[50%] text-center'>
-        <span className='mb-5'>
-          JRScode cria aplicações web e mobile buscando facilitar a vida das pessoas
-          e o dia a dia das empresas.
-        </span>
-        Nossa Missão: Criar solucões, melhorar experiência, propocionar resultados, levando
-        a pocibilidade de pequenos, medíos e grandes negócios pernamecer vivos na era digital.
-      </p>
+      <Button onClick={openWhatsApp} className="flex items-center gap-2 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg">
+      <FaWhatsapp size={20} /> Entar em contato 
+    </Button>
 
       <h4 className='absolute bottom-4'>copyright, all reseved ®JRScode 2023. </h4>
 
